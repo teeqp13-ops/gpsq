4fda0bd180812cdec46cd7e7cce111012175f500
teeqp13-ops <teeqp13@gmail.com>
2026-07-09 08:33:27 +0300

    Add EmployeeSwitch bridge header

---
+++ GPSQFramework/Headers/GPSQEmployeeSwitchBridge.h
@@ -0,0 +1,7 @@
+#pragma once
+#import <Foundation/Foundation.h>
+
+@interface GPSQEmployeeSwitchBridge : NSObject
++ (NSDictionary *)detectedFeatures;
++ (NSArray<NSString *> *)inspiredModules;
+@end
