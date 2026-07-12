39f74a97f3577b0dc470d50a14f4a4177f87ef08
teeqp13-ops <teeqp13@gmail.com>
2026-07-13 00:47:24 +0300

    Update GPSQ API endpoint to ipa.p3nd.fun/server/api

--- Headers/GPSQConfig.h
+++ Headers/GPSQConfig.h
@@ -1,3 +1,4 @@
 #pragma once
 
-#define GPSQ_ACTIVATION_URL @"https://ipa.p3nd.fun/gps/api/activate.php"
+#define GPSQ_API_BASE_URL @"https://ipa.p3nd.fun/server/api"
+#define GPSQ_ACTIVATION_URL GPSQ_API_BASE_URL @"/activate.php"
