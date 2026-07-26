903f29643d5197a6fd093834f8d33f0550ccad26
teeqp13-ops <teeqp13@gmail.com>
2026-07-26 03:27:29 +0300

    Add GPSQ activation configuration template

---
+++ activation/config.example.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'app_name' => 'GPSQ Activation',
+    'project_key' => 'gpsq',
+    'base_url' => 'https://key.p3nd.fun',
+    'database_path' => __DIR__ . '/../storage/activation.sqlite',
+    'session_ttl' => 2592000,
+    'device_limit' => 1,
+    'maintenance' => false,
+];
